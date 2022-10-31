806c37c418972f2bc36a3c2234592561fc519a1f
Gérald Hameau <gerald.hameau@gmail.com>
2022-11-01 00:32:16 +0100

    Add range for comparison with reference humidity

--- smartapps/gehdoc/keep-me-dry.src/keep-me-dry.groovy
+++ smartapps/gehdoc/keep-me-dry.src/keep-me-dry.groovy
@@ -20,19 +20,23 @@ definition(
   author: "Gérald Hameau",
   description: "Fan should switch on to dry the room, only if possible regarding the surrounding humidity level.",
   category: "Green Living",
-  iconUrl: "https://s3.amazonaws.com/smartapp-icons/Meta/temp_thermo.png",
+  iconUrl: "https://s3.amazonaws.com/smartapp-icons/Meta/temp_thermo@.png",
   iconX2Url: "https://s3.amazonaws.com/smartapp-icons/Meta/temp_thermo@2x.png"
 )
 
 preferences {
   section("Target this % of humidity in the room") {
     input "humiditySensor1", "capability.relativeHumidityMeasurement", title: 'Monitored sensor', required: true
-    input "humidityMin", "number", title: "turn off the fan when <", required: true, defaultValue: 54
-    input "humidityMax", "number", title: "turn on the fan when >=", required: true, defaultValue: 56
+    input "humidityMin", "number", range: "0..100", title: "turn off the fan when <", required: true, defaultValue: 54
+    input "humidityMax", "number", range: "0..100", title: "turn on the fan when >=", required: true, defaultValue: 56
   }
-  section("Cap the targeted % of humidity, relative to the surrounding % of humidity") {
+  section("Cap the targeted % of humidity") {
+  	paragraph "The targeted is not reachable, if the surrounding % of humidity is higher.\
+		So, let the fan stops when (monitored + offsetMin) < reference.\
+    	And, restarts when (monitored + offsetMax) >= reference."
     input "humiditySensorReference", "capability.relativeHumidityMeasurement", title: 'Reference sensor', required: true
-    input "referenceHumidityTreshold", "number", title: "Tolerated % offset : The fan will stop when (monitored + offset) < reference", required: true, defaultValue: 4
+    input "referenceHumidityOffsetMin", "number", range: "0..100", title: "Min % offset", required: true, defaultValue: 4
+    input "referenceHumidityOffsetMax", "number", range: "0..100", title: "Max % offset", required: true, defaultValue: 5
   }
   section("Control the fan") {
     input "switch1", "capability.switch", title: 'Fan control switch', required: true
@@ -57,18 +61,20 @@ def updated() {
 
 def humidityHandler(evt) {
   def currentHumidity = Integer.valueOf(humiditySensor1.currentValue('humidity'))
-  def referenceHumidity = Integer.valueOf(humiditySensorReference.currentValue('humidity')) + referenceHumidityTreshold
+  def referenceHumidityMin = Integer.valueOf(humiditySensorReference.currentValue('humidity')) + referenceHumidityOffsetMin
+  def referenceHumidityMax = Integer.valueOf(humiditySensorReference.currentValue('humidity')) + referenceHumidityOffsetMax
   
   log.debug "Current humidity: ${currentHumidity}"
   log.debug "Reference humidity: ${referenceHumidity}"
-  log.debug "Min: ${humidityMin}, Max: ${humidityMax}"
+  log.debug "Target: ${humidityMin}, Max: ${humidityMax}"
+  log.debug "Caped to : ${referenceHumidityMin}, Max: ${referenceHumidityMax}"
   
-  if (currentHumidity >= humidityMax && currentHumidity >= referenceHumidity) {
+  if (currentHumidity >= humidityMax && currentHumidity >= referenceHumidityMax) {
     log.debug "Current humidity Rose Above ${humidityMax} and ${referenceHumidity}: activating ${switch1}"
     switch1.on()
   }
 
-  if (currentHumidity < humidityMin || currentHumidity < referenceHumidity) {
+  if (currentHumidity < humidityMin || currentHumidity < referenceHumidityMin) {
     log.debug "Current humidity Fell Below ${humidityMin} or ${referenceHumidity}: disabling ${switch1}"
     switch1.off()
   }
